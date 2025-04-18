7eef80815de5b57e0851eb76f6191a4ed8781f3c
Deepak Maurya <deepak04122000maurya@gmail.com>
2025-04-18 06:40:45 +0530

    Routes & Layout Part Covered

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import Navbar from "@/app/components/Navbar";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         <main>{children}</main>
       </body>
     </html>
