8722bd20e9b00dd6dfd2303611a6bf3acab7fda5
Deepak Maurya <deepak04122000maurya@gmail.com>
2025-04-23 07:03:17 +0530

    pushing to shop page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,16 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useGlobalContext } from "../../../context/GlobalContext";
 
 const LoginPage = () => {
+  const router = useRouter();
+
   const { setIsLogin } = useGlobalContext();
+
   const login = () => {
     console.log("You are Log in Now");
     setIsLogin(true);
+    router.push("/shop");
   };
   return (
     <div>
