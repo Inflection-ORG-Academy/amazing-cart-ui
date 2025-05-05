ea6ee723e5a2e370c22abd608208b7fca1ac79e2
Deepak Maurya <deepak04122000maurya@gmail.com>
2025-05-05 06:52:45 +0530

    cookies code

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useGlobalContext } from "../../../context/GlobalContext";
+import { setCookie } from "../../../utils/cookies";
 
 const LoginPage = () => {
   const router = useRouter();
@@ -9,6 +10,7 @@ const LoginPage = () => {
 
   const login = () => {
     console.log("You are Log in Now");
+    setCookie("demot", "DeepakMaury");
     setIsLogin(true);
     router.push("/shop");
   };
